2ee887ef00546a91a07dd67f07b5d4a27dc54117
Michal Manak <fredybotas@gmail.com>
2019-01-12 18:36:48 +0100

    Harder version

--- game.js
+++ game.js
@@ -3,39 +3,44 @@ var x = jsboard.piece({ text: "X", fontSize: "40px", textAlign: "center" });
 
 var dim = 4;
 var clicked = null;
+var step_count = 10;
 
 function getPiece(num) {
     return jsboard.piece({ text: num.toString(), fontSize: "20px", textAlign: "center" }).clone();
 }
 
 
-var nums = Array(dim * dim).fill(1);
+var nums = Array(dim).fill(0).map(() => new Array(dim).fill(0));
+var neighbours = [[1, 0], [0, 1], [-1, 0], [0, -1]];
 
-//win = Math.floor((Math.random() * 15) + 5);
-win = 15;
+for(i = 0; i < step_count; i++){
+    let x = Math.floor(Math.random() * dim);
+    let y = Math.floor(Math.random() * dim);
+    for(i1 = 0; i1 < 20; i1++) {
+        let neigh_id = Math.floor(Math.random() * 4);
+        if( x + neighbours[neigh_id][0] >= 0 && x + neighbours[neigh_id][0] < 4 &&
+            y + neighbours[neigh_id][1] >= 0 && y + neighbours[neigh_id][1] < 4 ) {
 
-sum = (win * dim * dim) - (dim * dim);
-document.getElementById('win').innerHTML += 'Number to get: 0';
-
-while (sum != 0) {
-    id = Math.floor(Math.random() * dim * dim);
-    nums[id]++;
-    sum--;
+            let num_to_add = Math.floor(Math.random() * 10) + 1;
+            nums[x][y] += num_to_add;
+            nums[x + neighbours[neigh_id][0]][y + neighbours[neigh_id][1]] += (-num_to_add);
+            break;
+        }
+    }
 }
 
 for(i = 0; i < dim; i++){
     for(i1 = 0; i1 < dim; i1++){
-        nums[i * dim + i1] -= win;
-        b.cell([i, i1]).place(getPiece(nums[i * dim + i1]));
+        b.cell([i, i1]).place(getPiece(nums[i][i1]));
     }
 }
 
 console.log(nums.toString());
 
-$.getJSON('https://solver-makeitequal.herokuapp.com/astar', {'start_state': nums.toString(), 'final_state': 0, 'dim': dim}).done(function(data){
-    document.getElementById('count').innerHTML += 'Minimum steps: ' + data['length'];
-    document.getElementById('solution').innerHTML += 'Solution steps: ' + data['solution'].map(a => '['+a.join(", ") +']').join(" - ");
-});
+//$.getJSON('https://solver-makeitequal.herokuapp.com/astar', {'start_state': nums.toString(), 'final_state': 0, 'dim': dim}).done(function(data){
+//    document.getElementById('count').innerHTML += 'Minimum steps: ' + data['length'];
+//    document.getElementById('solution').innerHTML += 'Solution steps: ' + data['solution'].map(a => '['+a.join(", ") +']').join(" - ");
+//});
 
 
 var steps = 0;
@@ -46,6 +51,19 @@ b.cell("each").on("click", function() {
         b.cell(this).style({ background: "lightblue" });
     } else {
         b.cell(clicked).style({ background: "lightgray" });
+        let is_neighbour = false;
+        for(i = 0; i < 4; i ++){
+            if( clicked[0] === b.cell(this).where()[0] + neighbours[i][0] &&
+                clicked[1] === b.cell(this).where()[1] + neighbours[i][1]) {
+                is_neighbour = true;
+            }
+        }
+
+        if (!is_neighbour) {
+            clicked = null;
+            return;
+        }
+
         if ((Number(b.cell(clicked).get()) + Number(b.cell(this).get())) % 2 === 0){
             if(b.cell(clicked).get() !== b.cell(this).get()){
                 steps += 1;
@@ -56,7 +74,8 @@ b.cell("each").on("click", function() {
             document.getElementById('steps').innerHTML = 'Your steps: ' + steps;
 
         }
-        clicked = null;
+         clicked = null;
+
     }
 
 });
--- solver/solver.py
+++ solver/solver.py
@@ -34,6 +34,15 @@ def heuristic(begin, final, dim):
     return value
 
 
+def heuristic_new(begin):
+    count = 0
+    for a in begin:
+        for b in a:
+            if b == 0:
+                count += 1
+    return (16 - count) / 2
+
+
 def create_road(node):
     curr_node = node
     path = []
@@ -41,6 +50,78 @@ def create_road(node):
         path.append(curr_node.operator)
         curr_node = curr_node.parent
     return path[::-1]
+
+
+def astar_new(initial_state, final_state):
+    initial_state = [
+        [2, -2, 0, 8],
+        [6, -3, 4, -10],
+        [0, -3, -8, 6],
+        [-10, 10, 0, 0]
+    ]
+    initial_state_string = str(initial_state)
+
+    final_state = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+    result = []
+    open_queue = []
+    open_set = set()
+    closed_set = set()
+    distance = {}
+    neighbours = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+
+    open_queue.append((heuristic_new(initial_state), Node(initial_state, None, None)))
+    open_set.add(initial_state_string)
+    distance[initial_state_string] = 0
+
+    while len(open_queue) > 0:
+        curr_hvalue, curr_node = heappop(open_queue)
+        if(len(closed_set) > 500000):
+            break
+        if curr_node.state == final_state:
+            result = create_road(curr_node)
+            break
+        curr_state_string = str(curr_node.state)
+        if curr_state_string in closed_set:
+            continue
+        open_set.remove(curr_state_string)
+        closed_set.add(curr_state_string)
+
+        for x in range(len(curr_node.state)):
+            for y in range(len(curr_node.state)):
+                for x_n, y_n in neighbours:
+                    if x + x_n < 0 or x + x_n >= len(initial_state) or \
+                            y + y_n < 0 or y + y_n >= len(initial_state):
+                        continue
+                    if (curr_node.state[x][y] + curr_node.state[x + x_n][y + y_n]) % 2 != 0:
+                        continue
+
+                    new_state = copy_state(curr_node.state)
+                    new_state[x][y] = (curr_node.state[x][y] + curr_node.state[x + x_n][y + y_n]) / 2
+                    new_state[x + x_n][y + y_n] = (curr_node.state[x][y] +
+                                                   curr_node.state[x + x_n][y + y_n]) / 2
+
+                    new_state_string = str(new_state)
+
+                    if new_state_string in closed_set:
+                        continue
+                    path_value = distance[curr_state_string] + 1
+                    if new_state_string not in open_set:
+                        heappush(open_queue,(path_value + heuristic_new(new_state),
+                                             Node(new_state, curr_node, ((x, y), (x + x_n, y + y_n)))))
+                        open_set.add(new_state_string)
+                    elif path_value >= distance[new_state_string]:
+                        continue
+                    else:
+                        heappush(open_queue, (path_value + heuristic_new(new_state),
+                                              Node(new_state, curr_node, ((x, y), (x + x_n, y + y_n)))))
+                    distance[new_state_string] = path_value
+
+    return result
 
 
 def A_star(initial_state, final_state, dim):
@@ -111,6 +192,60 @@ def A_star(initial_state, final_state, dim):
     return result
 
 
+def copy_state(state):
+    res = []
+    for a in state:
+        res.append(a[:])
+    return res
+
+
+def idfs(initial_state, final_state):
+    initial_state = [
+        [2, -2, 0, 8],
+        [6, -3, 4, -10],
+        [0, -3, -8, 6],
+        [-10, 10, 0, 0]
+    ]
+
+    final_state = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+
+    neighbours = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+
+    for d in range(20):
+        closed_set = set()
+        open_set = set()
+        stack = [(0, initial_state)]
+        while(len(stack) > 0):
+            depth, curr = stack[-1]
+            stack = stack[:-1]
+            closed_set.add(str(curr))
+            if curr == final_state:
+                print('Got solution')
+                break
+            for x in range(len(initial_state)):
+                for y in range(len(initial_state)):
+                    for x_n, y_n in neighbours:
+                        if x + x_n < 0 or x + x_n >= len(initial_state) or \
+                                y + y_n < 0 or y + y_n >= len(initial_state):
+                            continue
+                        if (curr[x][y] + curr[x + x_n][y + y_n]) % 2 != 0:
+                            continue
+                        new_state = copy_state(curr)
+                        new_state[x][y] = (curr[x][y] + curr[x + x_n][y + y_n]) / 2
+                        new_state[x + x_n][y + y_n] = (curr[x][y] + curr[x + x_n][y + y_n]) / 2
+                        if str(new_state) in closed_set:
+                            continue
+                        if str(new_state) not in open_set and depth <= 11:
+                            stack.append((depth + 1, new_state))
+                            open_set.add(str(new_state))
+
+
+
 @app.route('/astar')
 def solve():
     dim = request.args.get('dim')
@@ -119,3 +254,6 @@ def solve():
     final_state = {int(request.args.get('final_state')): dim * dim}
     result = A_star(begin_state, final_state, dim)[1:]
     return jsonify({'solution': result, 'length': len(result)})
+
+print(astar_new([],[]))
+#idfs([],[])
